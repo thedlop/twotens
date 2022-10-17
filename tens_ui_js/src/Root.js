2a2aa5e25a16ec4737da85c6104c74c7915d1c70
Dark Lord of Programming <thedlop@sent.com>
2022-10-16 22:58:23 -0700

    Final UI done

--- tens_ui_js/src/Root.js
+++ tens_ui_js/src/Root.js
@@ -54,7 +54,7 @@ function Root() {
     const startingMinIrSize = bitLength(internalTokensCount);
     const startingValidIrSizes = generateValidIrSizes(startingMinIrSize);
     const [minIrSize, setMinIrSize] = useState(startingMinIrSize);
-    const [encoderVersion, setEncoderVersion] = useState("ehv-v1")
+    const [encoderVersion, setEncoderVersion] = useState("v1-ehv")
     const [message, setMessage] = useState("hello dark world");
     const [color, setColor] = useState("gray");
     const [beginEncoding, setBeginEncoding] = useState(false);
@@ -98,10 +98,43 @@ function Root() {
       }
 
       return(
-        <label>
-          <textarea value={message} onChange={handleChange} />
-        </label>
+        <div className="form-element-container">
+          <div>
+            Text to Encode
+            <br/>
+          </div>
+          <label>
+            <textarea value={message} onChange={handleChange} />
+          </label>
+        </div>
       );
+    }
+
+    function ColorForm() {
+      function handleChange(e) {
+        setColor(e.target.value);
+      }
+
+      function colorInput(colorValue) {
+        return (
+          <option
+            key={`ci-${colorValue}`}
+            value={colorValue}
+          >
+          {colorValue}
+          </option>
+        )
+      }
+
+      return (
+        <div className="form-element-container">
+          Color
+          <br/>
+          <select className="tens-color" value={color} onChange={handleChange}>
+          {colors.map(colorInput)}
+          </select>
+        </div>
+      )
     }
 
     function IrSizeForm() {
@@ -115,7 +148,7 @@ function Root() {
       function irSizeInput(irSizeValue) {
         let disabled = validIrSizes[0] > irSizeValue;
         return (
-          <div>
+          <div className="ir-size-ind-container" key={`irs-div-${irSizeValue}`}>
           <input 
             type='radio'
             name='irSize'
@@ -125,15 +158,18 @@ function Root() {
             onChange={handleChange}
             disabled={disabled}
           />
-          {`${irSizeValue} bit`}
-          <br/>
+          {`${irSizeValue}`}
           </div>
         )
       }
 
       return (
-        <div>
-        {irSizes.map(irSizeInput)}
+        <div className="form-element-container">
+        Internal Representation Size (Bits)
+        <br/>
+        <div className='ir-size-all-container'>
+          {irSizes.map(irSizeInput)}
+        </div>
         </div>
       )
 
@@ -142,26 +178,29 @@ function Root() {
     function Image() {
       if(imageData != null) {
         let dataUri = "data:image/png;base64, " + imageData;
-        let imageDataBytes = 8 * imageData.length
+        let imageDataBytes = imageData.length
         let scaledDataUri = "data:image/png;base64, " + scaledImageData;
         return(
-          <div>
-            <p> 1010s </p>
+          <div className='break-word' >
+            {Header()}
+            <h2> 1010s </h2>
             <img src={dataUri}/>
+            <h4> scaled (for viewing) </h4>
+            <img src={scaledDataUri}/>
             <p> Base 64 Size: {imageDataBytes} Bytes </p>
-            <p> Message: {message} </p>
             <p> Color: {color} </p>
             <p> IR Size: {irSize} </p>
             <p> Encoder: {encoderVersion} </p>
+            <p> Message: {message} </p>
             <p> Base 64 Image: {imageData} </p>
-            <p> scaled (for viewing) </p>
-            <img src={scaledDataUri}/>
+            {Footer()}
           </div>
         )
       } else {
         return(
-          <div>
-            Creating your 1010s... 
+          <div> 
+            {Header()}
+            <h2>Creating your 1010s ... </h2>
           </div>
         );
       }
@@ -176,11 +215,31 @@ function Root() {
 
       return(
         <form onSubmit={handleSubmit}>
-          <input type="submit" value="Create 1010s" />
+          <input type="submit" className="tens-submit" value="Create 1010s" />
         </form>
       )
     }
 
+    function Header() {
+      return(
+        <div>
+          <br/>
+          <br/>
+        </div>
+      )
+    }
+
+    function Footer() {
+      return(
+        <div>
+          <br/>
+          <br/>
+          <br/>
+          <h5> Built for ErgoHackV </h5>
+          <h5> By Dark Lord of Programming </h5>
+        </div>
+      )
+    }
 
     function rootBody(imageData) {
       if(beginEncoding) {
@@ -192,9 +251,15 @@ function Root() {
       } else {
         return(
           <div>
+            {Header()}
+            <h2> Create a 1010s </h2>
+            <h4> Encoder: {encoderVersion} </h4>
+            <br/>
             {MessageForm()}
             {IrSizeForm()}
+            {ColorForm()}
             {EncodeButton()}
+            {Footer()}
           </div>
         )
       }
